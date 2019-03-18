21420a02d6d69e56d965296f89c2d2a56fac3b8a
Theta Pi <thetapi@thetanil.com>
2019-03-18 16:04:09 +0000

    getting started

---
+++ boostrap.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+set -u
+
+LANG=en_US.UTF-8
+LANGUAGE=en_US.UTF-8
+LC_TIME=en_US.UTF-8
+LC_MONETARY=en_US.UTF-8
+LC_ADDRESS=en_US.UTF-8
+LC_TELEPHONE=en_US.UTF-8
+LC_NAME=en_US.UTF-8
+LC_MEASUREMENT=en_US.UTF-8
+LC_IDENTIFICATION=en_US.UTF-8
+LC_NUMERIC=en_US.UTF-8
+LC_PAPER=en_US.UTF-8
+LC_ALL=en_US.UTF-8
+
+EDITOR=vim
+PATH=$PATH:/thetapi/bin
+
+sudo sed -e '/en_GB/ s/^#*/#/' -i /etc/locale.gen
+sudo sed -e '/^#.*en_US\.UTF-8 /s/^#//' -i /etc/locale.gen
+echo 'LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8' | sudo tee /etc/default/locale
+sudo locale-gen "en_US.UTF-8"
+sudo update-locale
+
+# https://stackoverflow.com/questions/4565700
+# This key is only for development on the ALPI private developement network
+# It is safe to delete it anywhere else, and it should be removed from releases
+#export GIT_SSH_COMMAND='ssh -i /etc/alpi/private.key'
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y vim git stow
