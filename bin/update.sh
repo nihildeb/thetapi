88d9ffc3451b4dac5eeacb7e53a465283047036f
Theta Pi <thetapi@thetanil.com>
2019-03-19 00:01:33 +0000

    bashit

--- bin/update.sh
+++ bin/update.sh
@@ -2,4 +2,15 @@
 set -e
 set -u
 
-exit 0
+sudo systemctl disable hciuart
+sudo systemctl stop hciuart
+sudo systemctl disable bluetooth
+sudo systemctl stop bluetooth
+sudo systemctl disable avahi-daemon
+sudo systemctl stop avahi-daemon
+sudo systemctl disable triggerhappy
+sudo systemctl stop triggerhappy 
+
+echo '#bluetooth\nblacklist btbcm\nblacklist hci_uart\n' | sudo tee /etc/modprobe.d/thetapi-blacklist.conf
+
+exit
