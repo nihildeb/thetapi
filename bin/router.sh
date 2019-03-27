7aaf523c2fdfac2834b30bda3fc9f51869e92964
thetapi <thetapi@thetanil.com>
2019-03-27 12:12:10 +0000

    router not routing or tproxying

--- bin/router.sh
+++ bin/router.sh
@@ -15,7 +15,9 @@ fi
 
 # flush
 $IPT -F
+set +e # this errors if it's the first time we're running
 $IPT -X $LOG
+set -e
 
 # create logging table
 $IPT -N $LOG
@@ -26,14 +28,21 @@ $IPT -A INPUT -s $LOCALNET -d $LOCALIP -p tcp --dport 22 -j ACCEPT
 
 # Standard TCP stuff
 $IPT -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-$IPT -A FORWARD -i eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+$IPT -A INPUT -d $LOCALIP -s $LOCALNET -p icmp -j ACCEPT
+#$IPT -A FORWARD -i eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 $IPT -A OUTPUT -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
 # Accept localhost to localIP
-$IPT -A INPUT -d $LOCALIP -s 127.0.0.1 -j ACCEPT
+#$IPT -A INPUT -d $LOCALIP -s 127.0.0.1 -j ACCEPT
 $IPT -A INPUT -i lo -j ACCEPT
 
 # Accept Privoxy
 $IPT -A INPUT -d $LOCALIP -p tcp --dport 1080 -j ACCEPT
+# Transparently Proxy
+$IPT -t nat -A PREROUTING -p tcp --dport 443 -j REDIRECT --to-port 1080
+#$IPT -t mangle -A PREROUTING -p tcp --dport 80 -j TPROXY \
+  #--tproxy-mark 0x1/0x1 --on-port 1080
+#$IPT -t mangle -A PREROUTING -p tcp --dport 443 -j TPROXY \
+  #--tproxy-mark 0x1/0x1 --on-port 1080
 
 # nginx
 $IPT -A INPUT -d $LOCALIP -p tcp --dport 80 -j ACCEPT
@@ -43,13 +52,9 @@ $IPT -A INPUT -d $LOCALIP -p tcp --dport 443 -j ACCEPT
 $IPT -A INPUT -s $LOCALNET -p udp --dport 67 -j ACCEPT
 $IPT -A INPUT -s $LOCALNET -p udp --dport 53 -j ACCEPT
 
-# Accept pings on localnet
-$IPT -A INPUT -d $LOCALIP -s $LOCALNET -p icmp -j ACCEPT
-
-# Loosey Goosey ATM
 
 # Reject Local
-$IPT -A INPUT -s $LOCALNET -d $LOCALIP -j REJECT
+#$IPT -A INPUT -s $LOCALNET -d $LOCALIP -j REJECT
 
 # Dropbox is noisy AF, dump before logging
 $IPT -A INPUT -s $LOCALNET ! -d $LOCALIP -j DROP
@@ -59,6 +64,10 @@ $IPT -A INPUT -j $LOG
 $IPT -A FORWARD -j $LOG
 # Drop the logged packets
 $IPT -A $LOG -j DROP
+
+#sudo ip rule add fwmark 1 lookup 100
+#sudo ip route add local 0.0.0.0/0 dev lo table 100
+#sudo sysctl net.ipv4.ip_forward=1
 
 # Show what we've done
 $IPT -n -L -v --line-number
